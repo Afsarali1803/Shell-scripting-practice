51756f0eacb83554425ef2087e3e416e4913b3a6
= <=>
2022-12-29 11:34:36 +0530

    shell

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -6,11 +6,11 @@ echo $0   # Prints the script name that you're running
 echo $1   # First argument from the command line
 echo $2   # Second argument from the command line
 
-# echo $*   # Gives you all the arguments used in the script
-# echo $@   # Gives you all the arguments used in the script
-# echo $#   # Gves you the number of arguments users
-# echo $$   # Gives you the PID of the current process
-# echo $?   # Gives you the exit code the previous command
+echo $*   # Gives you all the arguments used in the script
+echo $@   # Gives you all the arguments used in the script
+echo $#   # Gves you the number of arguments users
+echo $$   # Gives you the PID of the current process
+echo $?   # Gives you the exit code the previous command
 
 # On command you can supply 9 variables 
 # Ex  
